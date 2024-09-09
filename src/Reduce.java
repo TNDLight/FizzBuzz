26df56afe5148ea7f299aca1c9f997587c304e74
Anthony Sun <tndlight@Anthonys-MacBook-Air-5.local>
2024-09-09 18:44:49 -0400

    refactored to extract helper method

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+public static void main(String[] args) {
+    int n = 100;
+    int steps = 0;
+    while (n != 0){
 
-public class Reduce {
+        //Find out if n is divisible by 2
+        boolean divisibleBy2 = n % 2 == 0;
+
+        if (divisibleBy2){
+            n = n / 2;
+        }
+        else{
+            n--;
+        }
+        steps++;
+    }
+    System.out.println(steps);
 }
